5aaa7ec232352ed76145c6ad8f8edef1b78d9372
Luan Lima <luanzin1167@gmail.com>
2023-03-18 16:18:17 -0300

    adds: Header to Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,22 @@
-import { Footer, Header } from "./components";
+import styles from "./Home.styles.module.css";
+
+import logoImg from "../../assets/truck-log-logo.svg";
 
 export const Home = () => {
   return (
     <>
-      <Header />
-      <h1>Conteúdo</h1>
-      <Footer />
+      <header className={styles.container}>
+        <div className={styles.header + " " + styles.content}>
+          <img src={logoImg} alt="Caminhão verde da TruckLog" />
+          <nav className={styles["header-nav"]}>
+            <a href="#">Nossos Dados</a>
+            <a href="#">Como funciona</a>
+            <a href="#">Benefícios</a>
+            <a href="#">FAQ</a>
+          </nav>
+          <button>Faça Login</button>
+        </div>
+      </header>
     </>
   );
 };
