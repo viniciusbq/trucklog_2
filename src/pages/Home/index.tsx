dd5884d4ada7b3f1f5d5dc0394d98a1460e34c6c
Luan Lima <luanzin1167@gmail.com>
2023-03-19 20:07:17 -0300

    adds: footer on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -303,6 +303,22 @@ export const Home = () => {
           </div>
         </div>
       </section>
+
+      <footer className={styles.container}>
+        <div className={styles.footer + " " + styles.content}>
+          <ul>
+            <li><a href="#">Benefícios</a></li>
+            <li><a href="#">Dados</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#"><img src={logoImg} alt="" /></a></li>
+            <li><a href="#">Parceiros</a></li>
+            <li><a href="#">Taxas</a></li>
+            <li><a href="#">Contrate</a></li>
+          </ul>
+          <hr />
+          <p>2010 - 2022 <a href="#">Privacy - Terms</a></p>
+        </div>
+      </footer>
     </>
   );
 };
