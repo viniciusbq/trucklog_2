479b8bb120023ead0c20d996af106b0e5928ef9b
Luan Lima <luanzin1167@gmail.com>
2023-03-19 22:07:43 -0300

    adds: tests to Forms on Home

---
+++ src/pages/Home/Form.spec.tsx
@@ -0,0 +1,23 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Home } from ".";
+
+export {};
+
+describe("Forms of HomePage", () => {
+  it("renders a name input", async () => {
+    render(<Home />);
+
+    let nameInput = screen.getByPlaceholderText(/digite aqui seu nome/i);
+
+    expect(nameInput).toBeInTheDocument();
+  });
+
+  it("renders a email input", async () => {
+    render(<Home />);
+
+    let emailInput = screen.getByPlaceholderText(/digite aqui seu e\-mail/i);
+
+    expect(emailInput).toBeInTheDocument();
+  });
+});
